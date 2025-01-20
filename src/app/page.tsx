e73f12b9d79136d3c9f1333dee2e77e5d3876db3
Divyanshu <dshapbss202@gmail.com>
2025-01-20 17:22:11 +0530

    removed everything except hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
-import { TracingBeamDemo } from '@/components/Section'
+
 
 export default function Home() {
   return (
